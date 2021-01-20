ed78c4b7b554a9c59a7966ff0210159e4b8c041f
RebelDroid09 <paulcs91@gmail.com>
2021-01-19 20:38:09 -0600

    Heatmap Work
    Added code for heatmap, and changed title of heatmap html page to better reflect contents of the page.

--- starter_code/static/js/logic.js
+++ starter_code/static/js/logic.js
@@ -18,12 +18,12 @@ function createMap(data) {
 
     console.log("In the create map function");
 
-    // var myBaseLayer = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    //     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    //     maxZoom: 18,
-    //     id: "streets-v11",
-    //     accessToken: API_KEY
-    // });       
+    var myBaseLayer = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "streets-v11",
+        accessToken: API_KEY
+    });       
     
     // myTileLayer.addTo(myMap);
 
@@ -51,7 +51,7 @@ function createMap(data) {
         valueField: 'count'
     };
 
-    //var heatmapLayer = new HeatmapOverlay(cfg);
+    var heatmapLayer = new HeatmapOverlay(cfg);
 
     // var quakeGroup = L.layerGroup(quakeMarkers);
 
@@ -64,10 +64,13 @@ function createMap(data) {
     //     collapsed: false
     // }).addTo(myMap);
 
-    // var myMap = L.map("mapid", {
-    //     center: usaCoords,
-    //     zoom: 3
-    // });
+    var myMap = L.map("mapid", {
+        center: usaCoords,
+        zoom: 3,
+        layers: [myBaseLayer, heatmapLayer]
+    });
+
+    heatmapLayer.setData(processedData);
 
     // // Create a legend to display information about our map
     // var info = L.control({
