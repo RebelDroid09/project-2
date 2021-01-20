235db154effa0e4cecb12d96aa424b6fa8660182
RebelDroid09 <paulcs91@gmail.com>
2021-01-19 19:50:53 -0600

    Added code files for the heatmap functionality
    Added javascript files to implement heatmap plugin, and commented some code out of the logic.js file for testing purposes.

--- starter_code/static/js/logic.js
+++ starter_code/static/js/logic.js
@@ -15,12 +15,15 @@ $.ajax("/heatmapData", {
 createMap(returnData);
 
 function createMap(data) {
-    var myBaseLayer = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "streets-v11",
-        accessToken: API_KEY
-    });       
+
+    console.log("In the create map function");
+
+    // var myBaseLayer = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    //     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    //     maxZoom: 18,
+    //     id: "streets-v11",
+    //     accessToken: API_KEY
+    // });       
     
     // myTileLayer.addTo(myMap);
 
@@ -48,7 +51,7 @@ function createMap(data) {
         valueField: 'count'
     };
 
-    var heatmapLayer = new HeatmapOverlay(cfg);
+    //var heatmapLayer = new HeatmapOverlay(cfg);
 
     // var quakeGroup = L.layerGroup(quakeMarkers);
 
